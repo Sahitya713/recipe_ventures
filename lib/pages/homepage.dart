818de51d7ed047e3c144be91c06594f4b499f24e
Chia Wen Cheng <wencheng1298@gmail.com>
2021-09-08 23:58:11 +0800

    Confirm ingredient

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,16 +1,11 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:focused_menu/focused_menu.dart';
 import 'package:focused_menu/modals.dart';
-import 'package:provider/provider.dart';
-import 'package:recipe_ventures/components/loading.dart';
-import 'package:recipe_ventures/controllers/recipeController.dart';
-import 'package:recipe_ventures/controllers/storeController.dart';
 import 'dart:ui';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
-import 'package:recipe_ventures/data/appUser.dart';
+import 'package:recipe_ventures/pages/ingredientConfirmationPage.dart';
 
 class Homepage extends StatefulWidget {
   @override
@@ -50,131 +45,128 @@ class _HomepageState extends State<Homepage> {
 
   @override
   Widget build(BuildContext context) {
-    final AppUser currUser = Provider.of<AppUser>(context);
-    User user = FirebaseAuth.instance.currentUser;
-    print(user.email);
-    return (currUser == null)
-        ? Loading()
-        : Scaffold(
-            appBar: AppBar(
-              centerTitle: true,
-              title: Text('Welcome ${currUser.displayName}',
-                  style: Theme.of(context).textTheme.headline6),
-            ),
-            body: Column(
-              children: [
-                // Container to tap for taking picture
-                SizedBox(
-                  height: 20,
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Welcome', style: Theme.of(context).textTheme.headline6),
+      ),
+      body: Column(
+        children: [
+          // Container to tap for taking picture
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 15.0),
+            child: FocusedMenuHolder(
+              menuOffset: 10,
+              onPressed: () {},
+              menuItems: [
+                FocusedMenuItem(
+                  title: Text('Take a picture'),
+                  onPressed: () => pickImageCamera(),
                 ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 25.0, right: 25.0, top: 15.0),
-                  child: FocusedMenuHolder(
-                    menuOffset: 10,
-                    onPressed: () {},
-                    menuItems: [
-                      FocusedMenuItem(
-                        title: Text('Take a picture'),
-                        onPressed: () => pickImageCamera(),
-                      ),
-                      FocusedMenuItem(
-                        title: Text('Upload from gallery'),
-                        onPressed: () => pickImageGallery(),
-                      ),
-                    ],
-                    menuWidth: MediaQuery.of(context).size.width - 50,
-                    openWithTap: true,
-                    blurBackgroundColor: Theme.of(context).backgroundColor,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: AspectRatio(
-                        aspectRatio: 1 / 1.15,
-                        child: image != null
-                            ? ClipRRect(
-                                borderRadius: BorderRadius.circular(20.0),
-                                child: Image.file(
-                                  image,
-                                  width: 100,
-                                  height: 400,
-                                  fit: BoxFit.cover,
-                                ),
-                              )
-                            : Container(
-                                width: 100,
-                                height: 400,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      image: NetworkImage(
-                                          'https://solidstarts.com/wp-content/uploads/when-can-babies-eat-eggs.jpg'),
-                                      fit: BoxFit.cover),
-                                ),
-                                child: Stack(
-                                  alignment: Alignment.center,
-                                  children: [
-                                    Icon(Icons.camera_alt_rounded,
-                                        size: 100, color: Colors.black),
-                                    BackdropFilter(
-                                      filter: ImageFilter.blur(
-                                          sigmaX: 0, sigmaY: 0),
-                                      child: Container(
-                                        color: Colors.grey.withOpacity(0.4),
-                                      ),
-                                    ),
-                                  ],
+                FocusedMenuItem(
+                  title: Text('Upload from gallery'),
+                  onPressed: () => pickImageGallery(),
+                ),
+              ],
+              menuWidth: MediaQuery.of(context).size.width - 50,
+              openWithTap: true,
+              blurBackgroundColor: Theme.of(context).backgroundColor,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: AspectRatio(
+                  aspectRatio: 1 / 1.15,
+                  child: image != null
+                      ? Image.file(
+                          image,
+                          width: 100,
+                          height: 400,
+                          fit: BoxFit.cover,
+                        )
+                      : Container(
+                          width: 100,
+                          height: 400,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://solidstarts.com/wp-content/uploads/when-can-babies-eat-eggs.jpg'),
+                                fit: BoxFit.cover),
+                          ),
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              Icon(Icons.camera_alt_rounded,
+                                  size: 100, color: Colors.black),
+                              BackdropFilter(
+                                filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
+                                child: Container(
+                                  color: Colors.grey.withOpacity(0.4),
                                 ),
                               ),
-                      ),
-                    ),
-                  ),
-                ),
-                image != null
-                    ? Padding(
-                        padding:
-                            const EdgeInsets.only(left: 25, right: 25, top: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            ElevatedButton(
-                              onPressed: () {
-                                print('add to store');
-                              },
-                              child: Text('Add to store'),
-                              style: ElevatedButton.styleFrom(
-                                  fixedSize: Size(170, 36),
-                                  primary: Theme.of(context).primaryColor),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                print('retake image');
-                              },
-                              child: Text('Retake Image'),
-                              style: ElevatedButton.styleFrom(
-                                  fixedSize: Size(170, 36),
-                                  primary: Theme.of(context).primaryColor),
-                            )
-                          ],
+                            ],
+                          ),
                         ),
-                      )
-                    : Container()
-                // Center(
-                //   // child: Text('upload Image')
-                //   child: GestureDetector(
-                //       onTap: () {
-                //         TestManager().getTest();
-                //       },
-                //       child: Text('upload Image',
-                //           style: Theme.of(context).textTheme.bodyText2)),
-                // ),
-                // GestureDetector(
-                //   onTap: () {
-                //     // RecipeManager().generateRecipes(["cheddar", "onion"]);
-                //     RecipeManager().getRecipe("1416203");
-                //   },
-                //   child: Text('Generate recipes'),
-                // )
-              ],
+                ),
+              ),
             ),
-          );
+          ),
+          image != null
+              ? Padding(
+                  padding: const EdgeInsets.only(left: 25, right: 25, top: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          // Get list of ingredient
+                          List ingredients = ['apple', 'pear', 'orange', 'Change the widget'];
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    IngredientConfirmationPage(
+                                        ingredients, image)),
+                          );
+                          print('add to store');
+                        },
+                        child: Text('Add to store'),
+                        style: ElevatedButton.styleFrom(
+                            fixedSize: Size(170, 36),
+                            primary: Theme.of(context).primaryColor),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          print('retake image');
+                        },
+                        child: Text('Retake Image'),
+                        style: ElevatedButton.styleFrom(
+                            fixedSize: Size(170, 36),
+                            primary: Theme.of(context).primaryColor),
+                      )
+                    ],
+                  ),
+                )
+              : Container()
+          // Center(
+          //   // child: Text('upload Image')
+          //   child: GestureDetector(
+          //       onTap: () {
+          //         TestManager().getTest();
+          //       },
+          //       child: Text('upload Image',
+          //           style: Theme.of(context).textTheme.bodyText2)),
+          // ),
+          // GestureDetector(
+          //   onTap: () {
+          //     // RecipeManager().generateRecipes(["cheddar", "onion"]);
+          //     RecipeManager().getRecipe("1416203");
+          //   },
+          //   child: Text('Generate recipes'),
+          // )
+        ],
+      ),
+    );
   }
 }
