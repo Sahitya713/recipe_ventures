8a28a4f418eaaae1ef74eefa1519499a679f5c76
jumboy <lim.jinyang@dhs.sg>
2021-10-15 15:31:31 +0800

    contents of recipe details added

--- lib/pages/recipedetails.dart
+++ lib/pages/recipedetails.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_ventures/components/recipeComponent.dart';
+import 'package:recipe_ventures/controllers/recipeController.dart';
+import 'package:recipe_ventures/data/recipe.dart';
 
 import '../main.dart';
 
@@ -15,6 +18,9 @@ class RecipeDetails extends StatefulWidget{
 
 class _RecipeDetailsState extends State<RecipeDetails> {
 
+  RecipeController rc = RecipeController();
+  var result;
+
   @override
   void initState() {
     super.initState();
@@ -25,14 +31,57 @@ class _RecipeDetailsState extends State<RecipeDetails> {
     super.dispose();
   }
 
+  Widget _getRecipesList(String recipeID)  {
+    return FutureBuilder(
+        future: rc.getRecipe(recipeID),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Scaffold(
+              body: SingleChildScrollView(
+                child: new Column(
+                  children: [
+                    new Container(
+                        child: Text(snapshot.data.title, style: Theme.of(context).textTheme.headline6)
+                    ),
+                    SizedBox(height: 10,),
+                    new Container(
+                      child: Image.network(snapshot.data.image),
+                    ),
+                    SizedBox(height: 10,),
+                    new Container(
+                      child: Text("Ingredients Needed", style: Theme.of(context).textTheme.headline6)
+                    ),
+                    SizedBox(height: 10,),
+                    new Container(
+                      child: Text(snapshot.data.ingredients.toString())
+                    ),
+                    SizedBox(height: 10,),
+                    new Container(
+                      child: Text("Instructions", style: Theme.of(context).textTheme.headline6)
+                    ),
+                    new Container(
+                      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                      child: Text(snapshot.data.instructions),
+                    ),
+                    SizedBox(height: 10,),
+                  ],
+                )
+              )
+            );
+          }
+          return Container();
+        });
+
+  }
+
   @override
   Widget build(BuildContext context){
-
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('Recipe Details', style: Theme.of(context).textTheme.headline6),
-      )
-    );
+      ),
+      body: _getRecipesList(widget.recipeID.toString())
+      );
   }
 }
