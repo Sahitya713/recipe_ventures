76f1f3fa2811c09721a9c5898d7a4c4587d2864b
Tan Hu Soon <husoon01@gmail.com>
2021-10-18 01:15:30 +0800

    Using Firestore Data for Favourites

--- lib/pages/favourites.dart
+++ lib/pages/favourites.dart
@@ -1,6 +1,9 @@
 import 'dart:core';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:recipe_ventures/controllers/favouritesController.dart';
 import 'package:recipe_ventures/data/favRecipe.dart';
+import 'package:recipe_ventures/utils/globals.dart' as globals;
 
 class Favourites extends StatefulWidget {
   @override
@@ -8,47 +11,49 @@ class Favourites extends StatefulWidget {
 }
 
 class _FavouritesState extends State<Favourites> {
-  final List<FavRecipe> items = [
-    FavRecipe(title: 'Mapo Tofu', id: '1'),
-    FavRecipe(title: 'Grandma\'s Homemade Cookies', id: '2'),
-    FavRecipe(title: 'Pork Cutlet', id: '3'),
-  ];
-  final bool _isInit = true;
-
+  FavouritesController fc = FavouritesController();
   @override
   Widget build(BuildContext context) {
+    // TODO: add comments to the favorites page.
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('Favourites', style: Theme.of(context).textTheme.headline6),
       ),
-      body: ListView.separated(
-        itemCount: items.length,
-        separatorBuilder: (BuildContext context, int index) => Divider(),
-        itemBuilder: (BuildContext context, int index) {
-          String title = items[index].getTitle();
-          FavRecipe fav = items[index];
-          bool isSaved = items.contains(fav);
-
-          return ListTile(
-              title: Text(title),
-              trailing: IconButton(
-                icon: Icon(
-                  isSaved ? Icons.favorite : Icons.favorite_border,
-                  color: isSaved ? Colors.red : null,
-                ),
-                onPressed: () {
-                  setState(() {
-                    if (isSaved) {
-                      // use when recipe page is up
-                      // deleteFavourite(String favRecipeID);
-                      items.remove(fav);
-                    } else {
-                      items.add(fav);
+      body: StreamBuilder(
+        stream: FavRecipe.getFavRecipes(globals.currUserId),
+        builder: (context, AsyncSnapshot<List<FavRecipe>> snapshot) {
+          return Column(
+            children: <Widget>[
+              Expanded(
+                child: ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, index) {
+                    if (snapshot.data == null) {
+                      return Container();
                     }
-                  });
-                },
-              ));
+                    FavRecipe favorites = snapshot.data[index];
+
+                    return ListTile(
+                      leading: IconButton(
+                          icon: Icon(Icons.favorite),
+                          onPressed: () {
+                            setState(() => {fc.deleteFavourite(favorites.id)});
+                          }),
+                      title: Text(snapshot.data[index].title),
+                      onTap: () {
+                        // Navigator.push(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //         builder: (context) =>
+                        //            )); //to recipe page
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
         },
       ),
     );
