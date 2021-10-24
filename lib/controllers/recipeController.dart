19ee1e10693ab86a4dc38a185daccb6de215b1a8
Bairi Sahitya <bairisahitya7@gmail.com>
2021-10-24 23:45:26 +0800

    updated generating recipes

--- lib/controllers/recipeController.dart
+++ lib/controllers/recipeController.dart
@@ -11,35 +11,100 @@ class RecipeController {
     "8f75676c728a4e7ebac546f628cb0dfa",
     "b9149f327a5642eeba443e957f4f1b23"
   ];
-  // generates recipes. pass in a list of ingredients. returns a json object with the recipes (curently returns 10)
 
-  Future generateRecipes(List<String> ingredients, {int keyId = 0}) async {
-    String key = keys[keyId];
-    var url = Uri.https('api.spoonacular.com', '/recipes/findByIngredients', {
-      "apiKey": key,
-      "ingredients": ingredients.join(","),
-      "number": "5",
-      "sort": "max-used-ingredients"
-    });
-    var response = await http.get(url);
-    if (response.statusCode == 200) {
-      String data = response.body;
-      // print(data);
-      var res = jsonDecode(data);
-      for (var i = 0; i < res.length; i++) {
-        bool isFav = await FavouritesController()
-            .isFavourite(res[i]["id"], FirebaseAuth.instance.currentUser.uid);
-        res[i]["isFavourite"] = isFav;
-      }
-      // print(res[5]["isFavourite"]);
-      return res;
-    } else {
-      print(response.statusCode);
-      if (response.statusCode == 402) {
-        return generateRecipes(ingredients, keyId: (keyId + 1) % 3);
-      }
+  Future generateRecipes(List<String> ingredients) async {
+    // String key = keys[keyId];
+    // var url = Uri.https('api.spoonacular.com', '/recipes/findByIngredients', {
+    //   "apiKey": key,
+    //   "ingredients": ingredients.join(","),
+    //   "number": "1",
+    //   "sort": "max-used-ingredients"
+    // });
+    // var response = await http.get(url);
+    var res = [];
+    var res1 = new Map();
+    var res2 = new Map();
+    var res3 = new Map();
+    var res4 = new Map();
+    var res5 = new Map();
+    res1["id"] = 640185;
+    res1["title"] = "Cottage pie with Quorn mince";
+    res2["id"] = 636962;
+    res2["title"] = "Caprese Quick Bread";
+    res3["id"] = 636769;
+    res3["title"] = "Mini Ham Omelets";
+    res4["id"] = 659459;
+    res4["title"] = "Savory Carrot Souffle";
+    res5["id"] = 640064;
+    res5["title"] = "Coriander Ravioli With Pumpkin & Cottage Cheese Filling";
+    res.add(res1);
+    res.add(res2);
+    res.add(res3);
+    res.add(res4);
+    res.add(res5);
+    for (var i = 0; i < res.length; i++) {
+      bool isFav = await FavouritesController()
+          .isFavourite(res[i]["id"], FirebaseAuth.instance.currentUser.uid);
+      res[i]["isFavourite"] = isFav;
+      // print(res[i]["id"]);
+      // print(res[i]["title"]);
+      // print(res[i]["isFavourite"]);
     }
+    return res;
+    // if (response.statusCode == 200) {
+    //   String data = response.body;
+    //   // print(data);
+    //   var res = jsonDecode(data);
+    //   for (var i = 0; i < res.length; i++) {
+    //     bool isFav = await FavouritesController()
+    //         .isFavourite(res[i]["id"], FirebaseAuth.instance.currentUser.uid);
+    //     res[i]["isFavourite"] = isFav;
+    //     print(res[i]["id"]);
+    //     print(res[i]["title"]);
+    //     print(res[i]["isFavourite"]);
+    //   }
+    //
+    //   // print(res[5]["isFavourite"]);
+    //   return res;
+    // } else {
+    //   print(response.statusCode);
+    //   if (response.statusCode == 402) {
+    //     return generateRecipes(ingredients, keyId: (keyId + 1) % 3);
+    //   }
+    // }
   }
+  // generates recipes. pass in a list of ingredients. returns a json object with the recipes (curently returns 10)
+  // Future generateRecipes(List<String> ingredients, {int keyId = 0}) async {
+  //   String key = keys[keyId];
+  //   var url = Uri.https('api.spoonacular.com', '/recipes/findByIngredients', {
+  //     "apiKey": key,
+  //     "ingredients": ingredients.join(","),
+  //     "number": "5",
+  //     "sort": "max-used-ingredients"
+  //   });
+  //   var response = await http.get(url);
+  //   if (response.statusCode == 200) {
+  //     String data = response.body;
+  //     // print(data);
+  //     var res = jsonDecode(data);
+  //     for (var i = 0; i < res.length; i++) {
+  //       bool isFav = await FavouritesController()
+  //           .isFavourite(res[i]["id"], FirebaseAuth.instance.currentUser.uid);
+  //       res[i]["isFavourite"] = isFav;
+  //       print(res[i]["id"]);
+  //       print(res[i]["title"]);
+  //       print(res[i]["isFavourite"]);
+  //     }
+  //
+  //     // print(res[5]["isFavourite"]);
+  //     return res;
+  //   } else {
+  //     print(response.statusCode);
+  //     if (response.statusCode == 402) {
+  //       return generateRecipes(ingredients, keyId: (keyId + 1) % 3);
+  //     }
+  //   }
+  // }
 
   // get recipe details by passing in recipe id. returns a Recipe class instance.
   Future getRecipe(String id, {int keyId = 0}) async {
